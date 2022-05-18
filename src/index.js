d6b0c75253128202d1103ba4ca306226c32e1865
aleksandrchuyko <chao.mess@gmail.com>
2022-05-18 19:01:31 +0300

    Fetch ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
+import debounce from "lodash.debounce";
+
+import {fetchCountries} from "./js/fetchCountries.js";
 
 const DEBOUNCE_DELAY = 300;
+
+const inputRef = document.querySelector('#search-box');
+
+
+
+inputRef.addEventListener('input', debounce((e) => {
+    const partOfCountryName = e.target.value;
+    fetchCountries(partOfCountryName)
+        .then(response => response.json())
+        .then(countries => {
+            console.table(countries);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}, DEBOUNCE_DELAY));
