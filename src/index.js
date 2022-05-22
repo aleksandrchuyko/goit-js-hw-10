0b6a807dca7b79dfffd38990439850675e06d28d
aleksandrchuyko <chao.mess@gmail.com>
2022-05-22 23:56:21 +0300

    UPD 1

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import './css/styles.css';
 import debounce from "lodash.debounce";
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import {fetchCountries} from "./js/fetchCountries.js";
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 3000;
 
 const inputRef = document.querySelector('#search-box');
 
 
 
 inputRef.addEventListener('input', debounce((e) => {
-    const partOfCountryName = e.target.value;
-    fetchCountries(partOfCountryName)
-        .then(response => response.json())
+    const partOfCountryName = (e.target.value).trim();
+    if (partOfCountryName) {
+        fetchCountries(partOfCountryName)
         .then(countries => {
-            console.table(countries);
+            if (countries.length > 10) {
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+            }
         })
         .catch(error => {
-            console.log(error);
+            console.log("!!!", error);
+            Notiflix.Notify.failure('Oops, there is no country with that name');
         });
+    }
+    
 }, DEBOUNCE_DELAY));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,8 @@
 export const fetchCountries = function(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`);
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else throw new Error('Oops, there is no country with that name');
+        });
 }
